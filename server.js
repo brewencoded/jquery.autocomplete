dcf30799a05dbbe852c0e79c58742048b373562c
victorrodriguez <vrodriguez5749@gmail.com>
2015-10-26 16:33:56 -0500

    created api server

---
+++ server.js
@@ -0,0 +1,58 @@
+/*jslint node: true */
+/*jshint node: true */
+'use strict';
+
+var express = require("express"),
+    app = express(),
+    fs = require('fs');
+
+//match entire word
+app.get('/api/search', function (req, res) {
+    var value = req.query.value;
+    search(req, res, value, 'word');
+});
+
+//match partial string
+app.get('/api/query', function (req, res) {
+    var value = req.query.value;
+    search(req, res, value, 'partial');
+});
+
+function search(req, res, value, matchBy) {
+    var value = req.query.value;
+    fs.readFile('autocomplete.json', 'utf8', function (err, data) {
+        if (err) {
+            return res.send(err);
+        }
+        var obj = JSON.parse(data);
+
+        if (value === '') {
+            var vals = Object.keys(obj).map(function (key) {
+                return obj[key];
+            });
+            res.json(vals);
+        } else {
+            var results = [];
+            if (matchBy === 'word') {
+                obj.forEach(function (item) {
+                    if (item.label === value) {
+                        results.push(item);
+                    }
+                });
+            } else {
+                obj.forEach(function (item) {
+                    console.log(item);
+                    if (item.label.indexOf(value) != -1) {
+                        results.push(item);
+                    }
+                });
+            }
+            res.json(results);
+        }
+    });
+}
+
+
+app.listen(3000, function () {
+    console.log("listening on port 3000");
+});
