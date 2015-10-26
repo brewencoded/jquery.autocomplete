dd79445363e1871e50fc0ef0224592216507ab9c
victorrodriguez <vrodriguez5749@gmail.com>
2015-10-26 16:30:50 -0500

    plugin complete

--- autocomplete.js
+++ autocomplete.js
@@ -1,21 +1,46 @@
-element.on('keyup', function () {
-    var doneTypingInterval = 1000; //time in ms, 5 second for example
-    clearTimeout(searchState.typingTimer);
-    searchState.typingTimer = setTimeout(function () {
-        $.ajax({
-            url: "",
-            data: {
-                "value": $('.search-box input').val()
-            },
-            method: 'get'
-        }).success(function (data) {
-            $('#search-results').children().remove();
-            for (result in data) {
-                var listing = $('<li class="listing"></li>');
-                listing.click(function () {
-                    //highlight for adding
-                });
+(function ($) {
+    'use strict';
+    $.fn.autocomplete = function (options) {
+        var opts = $.extend({
+            url: undefined,
+            callback: undefined,
+            searchfield: $(this)
+        }, $.fn.autocomplete.defaults, options);
+
+        var typingTimer;
+
+        if (opts.url === undefined) {
+            console.log('You did not provide a resource to search');
+            return null;
+        }
+        if (opts.callback === undefined) {
+            console.log('You have not specified a callback function');
+            return null;
+        }
+
+        $(this).on('keyup', function () {
+            if (typingTimer) {
+                clearTimeout(typingTimer);
             }
+            typingTimer = setTimeout(function () {
+                $.ajax({
+                    url: opts.url,
+                    data: {
+                        "value": opts.searchfield.val()
+                    },
+                    method: 'get'
+                }).success(function (data) {
+                    opts.callback(data);
+                });
+            }, opts.delay);
         });
-    }, doneTypingInterval);
-});
+
+        return this;
+    };
+
+    $.fn.autocomplete.defaults = {
+        event: 'keyup',
+        delay: 1000
+    };
+
+}(jQuery));
